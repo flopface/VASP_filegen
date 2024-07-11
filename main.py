58d6a1df2f2c9627072b6190f4b281b082716cad
Tom Melichar <flopface.nom@gmail.com>
2024-07-11 10:45:42 +0100

    completed poscar and added file reading and writing

--- main.py
+++ main.py
@@ -3,20 +3,61 @@
 import argparse
 import os
 
-def make_poscar(path, m_symbol, x_symbol, bond_length, nx, ny, nz):
+def yay_or_neigh(prompt):
+  yay = ['Y', 'Yes']
+  neigh = ['N', 'No']
+  while(True):
+    answer = input(prompt).capitalize()
+    if answer in yay: return True
+    if answer in neigh: return False
+    print('WARNING : input must be y or n')
 
-  file_content = f''
+def make_poscar(path, m_symbol, x_symbol, lattic_param, nx, ny, nz):
+
+  atoms = 4*nx*ny*nz
+  spaces_names = int(5-len(m_symbol))*' '
+  spaces_nums = int(4-len(str(atoms)))*' '
+
+  file_content = f'Auto generated rocksalt structure: {m_symbol}{x_symbol}\n1.0\n'\
+                 f'        {lattic_param*nx:#.11g}        0.0000000000        0.0000000000\n'\
+                 f'        0.0000000000        {lattic_param*ny:#.11g}        0.0000000000\n'\
+                 f'        0.0000000000        0.0000000000        {lattic_param*nz:#.11g}\n'\
+                 f'{m_symbol}{spaces_names}{x_symbol}\n'\
+                 f'{atoms}{spaces_nums}{atoms}\nDirect\n'
+  
+  m_locs = ''
+  x_locs = ''
+
+  for i in range(2*nx):
+    for j in range(2*ny):
+      for k in range(2*nz):
+        if (i + j + k) % 2 == 0:
+          m_locs += f'        {i/(2*nx):.10f}        {j/(2*ny):.10f}        {k/(2*nz):.10f}        #{m_symbol}\n'
+        else:
+          x_locs += f'        {i/(2*nx):.10f}        {j/(2*ny):.10f}        {k/(2*nz):.10f}        #{x_symbol}\n'
+  
+  file_content = file_content + m_locs + x_locs
 
   with open(os.path.join(path, 'POSCAR'), 'w') as file:
-    print('hi')
+    file.write(file_content)
 
- 
 def main(args):
-  print(f'Chemical formula: {args.m_symbol}{args.x_symbol}\nBond length: {args.bond_length:.3f} \u212b\nCell size: {args.nx}x{args.ny}x{args.nz}')
-  if args.encut: print(f'ENCUT = {args.encut}')
-  if args.kspacing: print(f'KSPACING = {args.kspacing}')
- 
-  print(os.getcwd())
+  print(f'Chemical formula: {args.m_symbol}{args.x_symbol}\nLattice parameter: {args.lattic_param:.3f} \u212b\nCell size: {args.nx}x{args.ny}x{args.nz}')
+
+  if args.directory:
+    path = args.directory
+    if os.path.isdir(args.directory): 
+      if not yay_or_neigh(f'WARNING : Directory \'{args.directory}\' already exists, overwrite? (y/n) : '): exit()
+    else: os.makedirs(args.directory)
+
+  else:
+    path = ''
+    for file in ['POSCAR', 'INCAR', 'POTCAR', 'run', 'OUTCAR', 'myJob.err', 'myJob.out', 'out.log']:
+      if os.path.isfile(file): 
+        if yay_or_neigh(f'WARNING : File \'{file}\' already exists, overwrite? (y/n) : '): os.remove(file)
+        else: exit()
+    
+  make_poscar(path, args.m_symbol, args.x_symbol, args.lattic_param, args.nx, args.ny, args.nz)
  
 if __name__ == '__main__':
  
@@ -24,11 +65,12 @@ if __name__ == '__main__':
  
   parser.add_argument('m_symbol', type = str, help = 'The chemical symbol of the metal, eg Ti')
   parser.add_argument('x_symbol', type = str, help = 'The chemical symbol of the non-metal, eg C')
-  parser.add_argument('bond_length', type = float, help = 'Distance between M and X atoms')
+  parser.add_argument('lattic_param', type = float, help = 'Distance between M atoms')
   parser.add_argument('nx', type = int, help = 'Number of unit cells in x direction')
   parser.add_argument('ny', type = int, help = 'Number of unit cells in y direction')
   parser.add_argument('nz', type = int, help = 'Number of unit cells in z direction')
- 
+  
+  parser.add_argument('-dir', '--directory', type = str, help = 'The directory for the input files to be stored in')
   parser.add_argument('-encut', '--encut', type = int, help = 'The highest energy considered in the electron pseudo-orbital expansion')
   parser.add_argument('-kspacing', '--kspacing', type = float, help = 'The spacing between sample points in reciprocal space')
  
